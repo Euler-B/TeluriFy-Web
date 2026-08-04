b3b7439b42fc77e359b537963339b1e714122af1
Eduardo Bravo <euler.csc@gmail.com>
2026-08-03 22:53:32 -0400

    build: migrate deployment adapter from @astrojs/node to @astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   vite: {
     optimizeDeps: {
       include: ['leaflet', 'react-leaflet', 'react-leaflet-cluster', 'react', 'react/jsx-runtime'],
